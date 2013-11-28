87236668a18464492915124c14beb7ab02f3d082
Keith Duncan <keith.duncan@github.com>
2013-11-28 22:46:41 +0000

    Consistent newlines and const

--- src/securetransport.c
+++ src/securetransport.c
@@ -6,42 +6,44 @@
 
 #pragma mark - RSA
 
-int _libssh2_rsa_new(libssh2_rsa_ctx ** rsa,
-                     const unsigned char *edata,
+int _libssh2_rsa_new(libssh2_rsa_ctx **rsa,
+                     unsigned char const *edata,
                      unsigned long elen,
-                     const unsigned char *ndata,
+                     unsigned char const *ndata,
                      unsigned long nlen,
-                     const unsigned char *ddata,
+                     unsigned char const *ddata,
                      unsigned long dlen,
-                     const unsigned char *pdata,
+                     unsigned char const *pdata,
                      unsigned long plen,
-                     const unsigned char *qdata,
+                     unsigned char const *qdata,
                      unsigned long qlen,
-                     const unsigned char *e1data,
+                     unsigned char const *e1data,
                      unsigned long e1len,
-                     const unsigned char *e2data,
+                     unsigned char const *e2data,
                      unsigned long e2len,
-                     const unsigned char *coeffdata, unsigned long coefflen) {
+                     unsigned char const *coeffdata,
+                     unsigned long coefflen) {
   return 0;
 }
 
-int _libssh2_rsa_new_private(libssh2_rsa_ctx ** rsa,
-                             LIBSSH2_SESSION * session,
-                             const char *filename,
-                             unsigned const char *passphrase) {
+int _libssh2_rsa_new_private(libssh2_rsa_ctx **rsa,
+                             LIBSSH2_SESSION *session,
+                             char const *filename,
+                             unsigned char const *passphrase) {
   return 0;
 }
 
-int _libssh2_rsa_sha1_verify(libssh2_rsa_ctx * rsa,
-                             const unsigned char *sig,
+int _libssh2_rsa_sha1_verify(libssh2_rsa_ctx *rsa,
+                             unsigned char const *sig,
                              unsigned long sig_len,
-                             const unsigned char *m, unsigned long m_len) {
+                             unsigned char const *m,
+                             unsigned long m_len) {
   return 0;
 }
 
-int _libssh2_rsa_sha1_sign(LIBSSH2_SESSION * session,
-                           libssh2_rsa_ctx * rsactx,
-                           const unsigned char *hash,
+int _libssh2_rsa_sha1_sign(LIBSSH2_SESSION *session,
+                           libssh2_rsa_ctx *rsactx,
+                           unsigned char const *hash,
                            size_t hash_len,
                            unsigned char **signature,
                            size_t *signature_len) {
@@ -50,55 +52,67 @@ int _libssh2_rsa_sha1_sign(LIBSSH2_SESSION * session,
 
 #pragma mark - DSA
 
-int _libssh2_dsa_new(libssh2_dsa_ctx ** dsa,
-                     const unsigned char *pdata,
+int _libssh2_dsa_new(libssh2_dsa_ctx **dsa,
+                     unsigned char const *pdata,
                      unsigned long plen,
-                     const unsigned char *qdata,
+                     unsigned char const *qdata,
                      unsigned long qlen,
-                     const unsigned char *gdata,
+                     unsigned char const *gdata,
                      unsigned long glen,
-                     const unsigned char *ydata,
+                     unsigned char const *ydata,
                      unsigned long ylen,
-                     const unsigned char *x, unsigned long x_len) {
+                     unsigned char const *x,
+                     unsigned long x_len) {
   return 0;
 }
 
-int _libssh2_dsa_new_private(libssh2_dsa_ctx ** dsa,
-                             LIBSSH2_SESSION * session,
-                             const char *filename,
-                             unsigned const char *passphrase) {
+int _libssh2_dsa_new_private(libssh2_dsa_ctx **dsa,
+                             LIBSSH2_SESSION *session,
+                             char const *filename,
+                             unsigned char const *passphrase) {
   return 0;
 }
 
-int _libssh2_dsa_sha1_verify(libssh2_dsa_ctx * dsactx,
-                             const unsigned char *sig,
-                             const unsigned char *m, unsigned long m_len) {
+int _libssh2_dsa_sha1_verify(libssh2_dsa_ctx *dsactx,
+                             unsigned char const *sig,
+                             unsigned char const *m,
+                             unsigned long m_len) {
   return 0;
 }
 
-int _libssh2_dsa_sha1_sign(libssh2_dsa_ctx * dsactx,
-                           const unsigned char *hash,
-                           unsigned long hash_len, unsigned char *sig) {
+int _libssh2_dsa_sha1_sign(libssh2_dsa_ctx *dsactx,
+                           unsigned char const *hash,
+                           unsigned long hash_len,
+                           unsigned char *sig) {
   return 0;
 }
 
 #pragma mark - Ciphers
 
-int _libssh2_cipher_init(_libssh2_cipher_ctx * h,
+int _libssh2_cipher_init(_libssh2_cipher_ctx *h,
                          _libssh2_cipher_type(algo),
                          unsigned char *iv,
-                         unsigned char *secret, int encrypt) {
+                         unsigned char *secret,
+                         int encrypt) {
   return 0;
 }
 
-int _libssh2_cipher_crypt(_libssh2_cipher_ctx * ctx,
+int _libssh2_cipher_crypt(_libssh2_cipher_ctx *ctx,
                           _libssh2_cipher_type(algo),
-                          int encrypt, unsigned char *block, size_t blocksize) {
+                          int encrypt,
+                          unsigned char *block,
+                          size_t blocksize) {
   return 0;
 }
 
 #pragma mark - Private Public Keys
 
-int _libssh2_pub_priv_keyfile(LIBSSH2_SESSION *session, unsigned char **method, size_t *method_len, unsigned char **pubkeydata, size_t *pubkeydata_len, const char *privatekey, const char *passphrase) {
+int _libssh2_pub_priv_keyfile(LIBSSH2_SESSION *session,
+                              unsigned char **method,
+                              size_t *method_len,
+                              unsigned char **pubkeydata,
+                              size_t *pubkeydata_len,
+                              char const *privatekey,
+                              char const *passphrase) {
   return 0;
 }
